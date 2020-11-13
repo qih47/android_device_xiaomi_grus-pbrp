351dec31b17daecbb086d212cb6f295802ed4920
Qisthi Iskandar Haqiki <qisthih@gmail.com>
2020-11-13 15:47:54 +0000

    Grus : Add Lunch Vendor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,4 @@ cd system/libvintf || return
 git reset -q --hard
 [ "$target" == "grus" ] && patch --merge -sNp1 -i "$workdir"/device/xiaomi/grus/patches/system-libvintf-disable-vbmeta-check.patch
 cd "$workdir" || return
+add_lunch_combo omni_grus-eng
